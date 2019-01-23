258733d3aaa88846206158fc22d248dd0b3fcbf1
zackeryfinn <zackeryfinn@gmail.com>
2019-01-23 00:13:17 -0500

    first time add

---
+++ ZackeryLab20/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ZackeryLab20.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+
+            return View();
+        }
+
+        public ActionResult Portfolio()
+        {
+
+            return View();
+        }
+    }
+}
